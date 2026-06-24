e080a2777f9a23e206651e1a5a30ea340e83e219
solitude <solitude5309@163.com>
2026-06-24 09:31:49 +0800

    docs: add frequency and adjustflag notes

--- test/test_stock_data_service.py
+++ test/test_stock_data_service.py
@@ -7,6 +7,22 @@ sys.path.append(str(service_dir))
 import StockDataService
 YomkApi.init(YomkApi.YomkServer(), ["YomkContext", "YomkFunctionPool", "YomkEventLoop"])
 YomkApi.new_service(StockDataService.StockDataService, "/StockDataService")
+
+
+# frequency:
+# "d" 日K
+# "w" 周K
+# "m" 月K  
+# "5" 5分钟K
+# "15" 15分钟K
+# "30" 30分钟K
+# "60" 60分钟K
+
+# adjustflag:
+# "1" 后复权
+# "2" 前复权
+# "3" 不复权
+
 res = YomkApi.request("/StockDataService/get_stock", 
                       {"code": "sz.002313", 
                        "start_date": "2020-01-01", 
